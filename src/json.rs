3ebc832c1e08bd958e6e55f4475c4bdda61a6e4d
tachibana51 <piyopiyo51@gmail.com>
2020-10-09 21:53:01 +0900

    feat: add string parser

--- src/json.rs
+++ src/json.rs
@@ -1,6 +1,6 @@
 pub struct Parser {
     pub input: String,
-    pos: u64,
+    pos: usize,
 }
 
 pub enum JSONValue {
@@ -12,6 +12,8 @@ pub enum JSONValue {
     Null,
 }
 
+enum JSONToken {}
+
 pub struct JSONObject {
     pub key: String,
     pub value: JSONValue,
@@ -19,6 +21,125 @@ pub struct JSONObject {
 
 pub struct JSON {
     pub value: Vec<JSONObject>,
+}
+
+impl Parser {
+    pub fn run(&mut self) {
+        unimplemented!()
+    }
+
+    fn count_whitespaces_from_pos(&self) -> usize {
+        let mut count = 0;
+        for c in self.input[self.pos..].chars() {
+            if !is_white_space(c) {
+                return count;
+            }
+            count += 1;
+        }
+        count
+    }
+
+    fn char_at(&self, idx: usize) -> char {
+        self.input[idx..].chars().next().unwrap()
+    }
+
+    fn next_token(&mut self) -> Option<JSONToken> {
+        unimplemented!()
+    }
+
+    //TODO: Error Handling
+    fn parse_string(&mut self) -> JSONValue {
+        let mut parsed_string = String::new();
+        let mut count = 0;
+        while self.pos + count < self.input.chars().count() {
+            let c = match self.char_at(self.pos + count) {
+                '"' => {
+                    if count != 0 {
+                        break;
+                    }
+                    count += 1;
+                    continue;
+                }
+                '\\' => match self.char_at(self.pos + count + 1) {
+                    'n' => {
+                        count += 1;
+                        '\n'
+                    }
+                    'r' => {
+                        count += 1;
+                        '\r'
+                    }
+                    't' => {
+                        count += 1;
+                        '\t'
+                    }
+                    '"' => {
+                        count += 1;
+                        '"'
+                    }
+                    '\\' => {
+                        count += 1;
+                        '\\'
+                    }
+                    _ => {
+                        count += 1;
+                        continue;
+                    }
+                },
+                other => other,
+            };
+            parsed_string.push(c);
+            count += 1;
+        }
+        self.pos += count + 1;
+        JSONValue::JString(parsed_string)
+    }
+
+    fn parse_number(&mut self) -> JSONValue {
+        let mut parsed_string = String::new();
+        let mut count = 0;
+        while self.pos + count < self.input.chars().count() {
+            let c = match self.char_at(self.pos + count) {
+                '+' | '-' => {
+                    count += 1;
+                    continue;
+                }
+                other => other,
+            };
+            parsed_string.push(c);
+            count += 1;
+        }
+        self.pos += count + 1;
+
+        JSONValue::Number(parsed_string.parse::<i64>().unwrap())
+    }
+
+    fn parse_bool() -> JSONValue {
+        unimplemented!()
+    }
+
+    fn parse_value() -> JSONValue {
+        unimplemented!()
+    }
+
+    fn parse_array() -> JSONValue {
+        unimplemented!()
+    }
+
+    fn parse_object() -> JSONValue {
+        unimplemented!()
+    }
+}
+
+fn is_white_space(c: char) -> bool {
+    c == ' ' || c == '\n' || c == '\t' || c == '\r'
+}
+
+pub fn new_parser(input: String) -> Parser {
+    Parser {
+        input: input,
+        pos: 0,
+    }
 }
 
 pub fn draft() {
@@ -50,4 +171,6 @@ pub fn draft() {
             value: JSONValue::Array(vec![]),
         }],
     };
+    //let mut p = new_parser(r#"{"taro": 12,"jiro": {"saburo": "shiro"},"goro": true}"#.to_owned());
+    let mut p = new_parser(r#"{"taro": 12,"jiro": {"saburo": "shiro"},"goro": true}"#.to_owned());
 }
